a037e1efc77d2d767de0ed53fafebcd02a15795a
Jiazheng <jasonhjz9527@gmail.com>
2022-04-14 14:22:44 -0700

    Init for FrontEnd
    Init for the front end using React

---
+++ yolo-web/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+    return ( <
+        div lcassName = "App" >
+        <
+        h1 > YOLO Welcome Page! < /h1> <
+        /div>
+    );
+}
+
+export default App;
